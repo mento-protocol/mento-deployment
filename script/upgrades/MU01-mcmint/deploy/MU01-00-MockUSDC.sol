00632e8f84708e97abaaa4597067e066140e1a3c
philbow61 <philip.raetsch@gmail.com>
2023-02-08 18:32:07 +0100

    feat: deployed mock usdc on alfajores

--- script/upgrades/MU01-mcmint/deploy/MU01-00-MockUSDC.sol
+++ script/upgrades/MU01-mcmint/deploy/MU01-00-MockUSDC.sol
@@ -7,17 +7,17 @@ import { console2 } from "forge-std/Script.sol";
 import { MockUSDC } from "contracts/MockUSDC.sol";
 
 contract MU01_MockUSDC is Script {
-    function run() public {
-        address mockUSDC;
+  function run() public {
+    address mockUSDC;
 
-        vm.startBroadcast(Chain.deployerPrivateKey());
-        {
-            mockUSDC = address(new MockUSDC());
-        }
-        vm.stopBroadcast();
-    
-        console2.log("----------");
-        console2.log("MockUSDC deployed at: ", mockUSDC);
-        console2.log("----------");
+    vm.startBroadcast(Chain.deployerPrivateKey());
+    {
+      mockUSDC = address(new MockUSDC());
     }
+    vm.stopBroadcast();
+
+    console2.log("----------");
+    console2.log("MockUSDC deployed at: ", mockUSDC);
+    console2.log("----------");
+  }
 }
