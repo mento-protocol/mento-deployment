3166654656a522294297f3841c97256380ba4e8d
philbow61 <philip.raetsch@gmail.com>
2023-02-09 17:26:54 +0100

    refactor: use ownable

--- contracts/MockUSDC.sol
+++ contracts/MockUSDC.sol
@@ -2,21 +2,17 @@ pragma solidity ^0.5.13;
 
 import { ERC20 } from "lib/mento-core/lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import { ERC20Detailed } from "lib/mento-core/lib/openzeppelin-contracts/contracts/token/ERC20/ERC20Detailed.sol";
+import { Ownable } from "lib/mento-core/lib/openzeppelin-contracts/contracts/ownership/Ownable.sol";
 
-contract MockUSDC is ERC20, ERC20Detailed {
-  address public owner;
 
-  modifier onlyOwner() {
-    require(msg.sender == owner, "only owner");
-    _;
-  }
+contract MockUSDC is ERC20, ERC20Detailed, Ownable {
 
   constructor() public ERC20Detailed("mockUSDC", "USDC", 18) {
-    owner = msg.sender;
-    mint(owner, 1000000 ether);
+    mint(msg.sender, 1000000 ether);
   }
 
-  function mint(address to, uint256 amount) public onlyOwner {
+  function mint(address to, uint256 amount) public onlyOwner returns (bool){
     _mint(to, amount);
+    return true;
   }
 }
