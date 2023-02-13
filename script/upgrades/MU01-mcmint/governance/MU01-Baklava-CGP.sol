74d50c2edc7535db3c01c1098ec325a1fa0fd55a
Nina Barbakadze <barbakadzeninaa@gmail.com>
2023-02-13 10:32:40 +0100

    chore: add LICENSE and license everything

--- script/upgrades/MU01-mcmint/governance/MU01-Baklava-CGP.sol
+++ script/upgrades/MU01-mcmint/governance/MU01-Baklava-CGP.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0-or-later
 // solhint-disable func-name-mixedcase, contract-name-camelcase
 pragma solidity ^0.5.13;
 pragma experimental ABIEncoderV2;
