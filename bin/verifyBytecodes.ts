9a98589ce685e3b41b39cbbe69b0e92cb168246d
Nelson Taveras <nelson@clabs.co>
2023-03-07 17:32:09 +0100

    PR comments

--- bin/verifyBytecodes.ts
+++ bin/verifyBytecodes.ts
@@ -1,43 +1,52 @@
 import { execSync } from "node:child_process";
 import fs from "node:fs";
+import { parseArgs } from "node:util";
 import process from "node:process";
 import { providers } from "ethers";
 
-const broadcastFiles = [
-  "broadcast/MU01-00-Create-Proxies.sol",
-  "broadcast/MU01-01-Create-Nonupgradeable-Contracts.sol",
-  "broadcast/MU01-02-Create-Implementations.sol",
-];
-
-const providerByNetworkId = {
-  62320: "https://baklava-forno.celo-testnet.org",
-  44787: "https://alfajores-forno.celo-testnet.org",
-  42220: "https://forno.celo.org",
+const networkInfoByName = {
+  baklava: {
+    id: 62320,
+    forno: "https://baklava-forno.celo-testnet.org",
+  },
+  alfajores: {
+    id: 44787,
+    forno: "https://alfajores-forno.celo-testnet.org",
+  },
+  celo: {
+    id: 42220,
+    forno: "https://forno.celo.org",
+  },
 };
 
-function readAddresses(addressesPath: string): string[] {
-  const data = fs.readFileSync(addressesPath, "utf8");
-  return data.split("\n").filter(line => line.length > 0);
-}
+function getContractsForUpgrade(network: string, upgrade: string): Map<string, string> {
+  const broadcastFolders = fs
+    .readdirSync("broadcast/", { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .filter(dirent => dirent.name.startsWith(upgrade));
 
-function getKnownContracts(network: string): Map<string, string> {
-  const contractAddressToName = new Map<string, string>();
+  if (broadcastFolders.length === 0) {
+    throw new Error(`No broadcast folders found for upgrade ${upgrade}`);
+  }
 
-  for (const file of broadcastFiles) {
-    const path = `${file}/${network}/run-latest.json`;
-    const data = JSON.parse(fs.readFileSync(path, "utf8"));
+  const networkId = networkInfoByName[network as keyof typeof networkInfoByName].id;
+  const contractNameToAddress = new Map<string, string>();
+  for (const folder of broadcastFolders) {
+    const runFile = `broadcast/${folder.name}/${networkId}/run-latest.json`;
+    const data = JSON.parse(fs.readFileSync(runFile, "utf8"));
 
     for (const transaction of data.transactions) {
       if (transaction.transactionType == "CREATE") {
-        contractAddressToName.set(transaction.contractAddress, transaction.contractName);
+        contractNameToAddress.set(transaction.contractName, transaction.contractAddress);
       }
     }
   }
-  return contractAddressToName;
+
+  return contractNameToAddress;
 }
 
-async function getOnChainBytecode(address: string, networkId: Number): Promise<string> {
-  const provider = new providers.JsonRpcProvider(providerByNetworkId[networkId as keyof typeof providerByNetworkId]);
+async function getOnChainBytecode(address: string, network: string): Promise<string> {
+  const provider = new providers.JsonRpcProvider(networkInfoByName[network as keyof typeof networkInfoByName].forno);
   return await provider.getCode(address);
 }
 
@@ -57,60 +66,81 @@ function executeAndFailOnError(command: string) {
   }
 }
 
-async function fun() {
-  if (process.argv.length != 5) {
-    console.log("Usage: yarn run verifyBytecodes <networkId> <commit> <addressesFile>");
+async function main() {
+  const { values } = parseArgs({
+    options: {
+      network: {
+        type: "string",
+        short: "n",
+      },
+      upgrade: {
+        type: "string",
+        short: "u",
+      },
+      commit: {
+        type: "string",
+        short: "c",
+      },
+    },
+  });
+
+  if (Object.keys(values).length != 3) {
+    console.log("Usage: yarn run verifyBytecodes -n <network> -u <upgrade> -c <commit>");
     process.exit(1);
   }
 
-  const network = process.argv[2];
-  if (!Object.keys(providerByNetworkId).includes(network)) {
+  const network = values.network!;
+  const upgrade = values.upgrade!;
+  const commit = values.commit!;
+
+  if (!Object.keys(networkInfoByName).includes(network)) {
     console.log(`Unknown network id: ${network}`);
     process.exit(1);
-  }
-
-  const commit = process.argv[3];
-  const addressesFilePath = process.argv[4];
-
-  const knownContracts: Map<string, string> = getKnownContracts(network);
-  const addressesToVerify = readAddresses(addressesFilePath);
-
-  for (const address of addressesToVerify) {
-    if (!knownContracts.has(address)) {
-      console.log(`Unknown contract address: ${address}`);
-      process.exit(1);
-    }
   }
 
   console.log(`Checking out lib/mento-core submodule @ ${commit}...`);
   executeAndFailOnError(`git -C lib/mento-core checkout ${commit} -q`);
 
   console.log("Cleaning old artifacts and building new ones...");
-  executeAndFailOnError("forge clean && forge build");
+  executeAndFailOnError(`forge clean && env FOUNDRY_PROFILE=${network}-deployment forge build`);
 
   console.log("\n========================================");
-  console.log("Verifying provided contract addresses...\n");
+  console.log("Verifying contract addresses...");
+
+  const contractsByName: Map<string, string> = getContractsForUpgrade(network, upgrade);
+  const nOfContracts = contractsByName.size;
+  const tableOutput: Array<object> = [];
 
   let misMatches = 0;
-  for (const address of addressesToVerify) {
-    const contractName = knownContracts.get(address)!;
+  let checked = 0;
+  for (const [contractName, contractAddress] of contractsByName) {
+    checked = checked + 1;
+    process.stdout.clearLine(0);
+    process.stdout.cursorTo(0);
+    process.stdout.write(`${contractName} (${checked}/${nOfContracts})...`);
 
-    const onChainBytecode = await getOnChainBytecode(address, parseInt(network));
+    const onChainBytecode = await getOnChainBytecode(contractAddress, network);
     const bytecodeFromArtifacts = getBytecodeFromArtifacts(contractName);
 
     if (onChainBytecode === bytecodeFromArtifacts) {
-      console.log(`${contractName} @ ${address} ✅`);
+      tableOutput.push({ contract: contractName, address: contractAddress, status: "✅" });
     } else {
       misMatches = misMatches + 1;
-      console.log(`${contractName} @ ${address} ❌`);
+      tableOutput.push({ contract: contractName, address: contractAddress, status: "❌" });
     }
   }
 
+  process.stdout.clearLine(0);
+  console.log("\n");
+  console.table(tableOutput);
+
   if (misMatches === 0) {
-    console.log(`\nAll ${addressesToVerify.length} contracts bytecodes match 🚀`);
+    console.log(`\nAll ${nOfContracts} contracts bytecodes match 🚀`);
   } else {
-    console.log(`\nFound ${misMatches}/${addressesToVerify.length} contracts mismatches ❌`);
+    console.log(`\nFound ${misMatches}/${nOfContracts} contracts mismatches ❌`);
   }
 }
 
-fun();
+main()
+  .then(() => process.exit(0))
+  .catch(e => console.error("Error:", e));
