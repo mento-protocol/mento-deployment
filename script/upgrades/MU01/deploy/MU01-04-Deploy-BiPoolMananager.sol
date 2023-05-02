cda84b215c3e48d43b2b3bde5b6b552e4208b1f3
Nina Barbakadze <barbakadzeninaa@gmail.com>
2023-05-02 18:01:35 +0200

    chore: deploy updated bipoolmanager

---
+++ script/upgrades/MU01/deploy/MU01-04-Deploy-BiPoolMananager.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.5.13;
+
+import { Script } from "script/utils/Script.sol";
+import { Chain } from "script/utils/Chain.sol";
+import { console2 } from "forge-std/Script.sol";
+
+import { BiPoolManager } from "mento-core/contracts/BiPoolManager.sol";
+
+/*
+ forge script MU01_CreateImplementations --rpc-url $RPC_URL
+                             --broadcast --legacy 
+                             --verify --verifier sourcify 
+*/
+contract MU01_CreateImplementations is Script {
+  function run() public {
+    address biPoolManager;
+
+    vm.startBroadcast(Chain.deployerPrivateKey());
+    {
+      // Updated implementation
+      biPoolManager = address(new BiPoolManager(false));
+    }
+    vm.stopBroadcast();
+
+    console2.log("----------");
+    console2.log("BiPoolManager deployed at: ", biPoolManager);
+    console2.log("----------");
+  }
+}
