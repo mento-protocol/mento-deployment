73862b58e5c3653d501ce0d0cd898ed478c6ec37
boqdan <304771+bowd@users.noreply.github.com>
2023-05-09 06:42:33 +0100

    release/celo-phase-1 leftovers (#54)
    - The mento-core submodule updated to tags/v2.0.0
    - The broadcast of the CGP submission.
    - Refactoring of the ownership transfer script to do all proxies + implementations - the script was re-ran in this form during the governance approval process because some of the implementations were still owned by our address.
    - Adding the correct link to the markdown file on CGP submission.

--- script/upgrades/MU01/deploy/MU01-03-Governance-Ownership-Transfer.sol
+++ script/upgrades/MU01/deploy/MU01-03-Governance-Ownership-Transfer.sol
@@ -5,12 +5,9 @@ import { Script } from "script/utils/Script.sol";
 import { Chain } from "script/utils/Chain.sol";
 import { console2 } from "forge-std/Script.sol";
 
-import { BreakerBoxProxy } from "mento-core/contracts/proxies/BreakerBoxProxy.sol";
-import { BiPoolManagerProxy } from "mento-core/contracts/proxies/BiPoolManagerProxy.sol";
-import { BrokerProxy } from "mento-core/contracts/proxies/BrokerProxy.sol";
-import { MedianDeltaBreaker } from "mento-core/contracts/MedianDeltaBreaker.sol";
-import { ValueDeltaBreaker } from "mento-core/contracts/ValueDeltaBreaker.sol";
-import { PartialReserveProxy } from "contracts/PartialReserveProxy.sol";
+import { Ownable } from "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import { Proxy } from "mento-core/contracts/common/Proxy.sol";
+
 
 /*
  forge script MU01_GovernanceOwnershipTransfer --rpc-url $RPC_URL
@@ -21,32 +18,48 @@ contract MU01_GovernanceOwnershipTransfer is Script {
   function run() public {
     contracts.load("MU01-00-Create-Proxies", "latest");
     contracts.load("MU01-01-Create-Nonupgradeable-Contracts", "latest");
-    address payable breakerBoxProxy = address(uint160(contracts.deployed("BreakerBoxProxy")));
-    address payable biPoolManagerProxy = address(uint160(contracts.deployed("BiPoolManagerProxy")));
-    address payable brokerProxy = address(uint160(contracts.deployed("BrokerProxy")));
-    address payable medianDeltaBreaker = address(uint160(contracts.deployed("MedianDeltaBreaker")));
-    address payable valueDeltaBreaker = address(uint160(contracts.deployed("ValueDeltaBreaker")));
-    address payable partialReserveProxy = address(uint160(contracts.deployed("PartialReserveProxy")));
+    contracts.load("MU01-02-Create-Implementations", "latest");
+
+    string[4] memory proxies = [
+      "BreakerBoxProxy",
+      "BiPoolManagerProxy",
+      "PartialReserveProxy",
+      "BrokerProxy"
+    ];
+    string[9] memory ownables = [
+      "BreakerBox",
+      "BiPoolManager",
+      "MedianDeltaBreaker",
+      "ValueDeltaBreaker",
+      "Broker",
+      "StableToken",
+      "StableTokenEUR",
+      "StableTokenBRL",
+      "Reserve"
+    ];
+
     address governance = contracts.celoRegistry("Governance");
 
     vm.startBroadcast(Chain.deployerPrivateKey());
     {
-      BreakerBoxProxy(breakerBoxProxy)._transferOwnership(governance);
-      BiPoolManagerProxy(biPoolManagerProxy)._transferOwnership(governance);
-      BrokerProxy(brokerProxy)._transferOwnership(governance);
-      MedianDeltaBreaker(medianDeltaBreaker).transferOwnership(governance);
-      ValueDeltaBreaker(valueDeltaBreaker).transferOwnership(governance);
-      PartialReserveProxy(partialReserveProxy)._transferOwnership(governance);
+      for (uint256 i = 0; i < proxies.length; i++) {
+        address payable proxyAddy = contracts.deployed(proxies[i]);
+        Proxy proxy = Proxy(proxyAddy);
+        if (proxy._getOwner() != governance) {
+          console2.log("%s(%s) ownership transferred to %s", proxies[i], proxyAddy, governance);
+          proxy._transferOwnership(governance);
+        }
+      }
+
+      for (uint256 i = 0; i < ownables.length; i++) {
+        address ownableAddy = contracts.deployed(ownables[i]);
+        Ownable ownable = Ownable(ownableAddy);
+        if (ownable.owner() != governance) {
+          console2.log("%s(%s) ownership transferred to %s", ownables[i], ownableAddy, governance);
+          ownable.transferOwnership(governance);
+        }
+      }
     }
     vm.stopBroadcast();
-
-    console2.log("----------");
-    console2.log("BrokerProxy(%s) ownership transferred to %s", brokerProxy, governance);
-    console2.log("BiPoolManagerProxy(%s) ownership transferred to %s", biPoolManagerProxy, governance);
-    console2.log("BreakerBoxProxy(%s) ownership transferred to %s", breakerBoxProxy, governance);
-    console2.log("MedianDeltaBreaker(%s) ownership transferred to %s", medianDeltaBreaker, governance);
-    console2.log("ValueDeltaBreaker(%s) ownership transferred to %s", valueDeltaBreaker, governance);
-    console2.log("PartialReserveProxy(%s) ownership transferred to %s", partialReserveProxy, governance);
-    console2.log("----------");
   }
 }
