0bdae071d8617e8464721498c137e94906c73eac
philbow61 <80156619+philbow61@users.noreply.github.com>
2023-07-31 15:13:45 +0200

    feat: deploy xof token to testnets (#98)
    * feat: deploy xof token to testnets
    
    * chore: resolve merge conflicts
    
    * feat: add ownership transfer to MU04 deployment scripts
    
    * feat: add ownership transfer of proxys to MU04 deployment scripts
    
    * Update script/upgrades/MU04/deploy/MU04-01-Create-Implementations.sol
    
    Co-authored-by: nina / ნინა <barbakadzeninaa@gmail.com>
    
    ---------
    
    Co-authored-by: boqdan <304771+bowd@users.noreply.github.com>
    Co-authored-by: nina / ნინა <barbakadzeninaa@gmail.com>

---
+++ script/upgrades/MU04/deploy/MU04-00-Create-Proxies.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.5.13;
+
+import { Script } from "script/utils/Script.sol";
+import { Chain } from "script/utils/Chain.sol";
+import { console2 } from "forge-std/Script.sol";
+
+import { StableTokenXOFProxy } from "mento-core-2.2.0/legacy/proxies/StableTokenXOFProxy.sol";
+
+/*
+ yarn deploy -n <network> -u MU04 -s MU04-00-Create-Proxies.sol
+*/
+contract MU04_CreateProxies is Script {
+  function run() public {
+    address payable stableTokenXOFProxy;
+    address governance = contracts.celoRegistry("Governance");
+
+    vm.startBroadcast(Chain.deployerPrivateKey());
+    {
+      stableTokenXOFProxy = address(new StableTokenXOFProxy());
+      StableTokenXOFProxy(stableTokenXOFProxy)._transferOwnership(governance);
+    }
+    vm.stopBroadcast();
+
+    console2.log("----------");
+    console2.log("StableTokenXOFProxy deployed at: ", stableTokenXOFProxy);
+    console2.log("StableTokenXOFProxy(%s) ownership transferred to %s", stableTokenXOFProxy, governance);
+    console2.log("----------");
+  }
+}
---
+++ script/upgrades/MU04/deploy/MU04-01-Create-Implementations.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.5.13;
+
+import { Script } from "script/utils/Script.sol";
+import { Chain } from "script/utils/Chain.sol";
+import { console2 } from "forge-std/Script.sol";
+
+import { StableTokenXOF } from "mento-core-2.2.0/legacy/StableTokenXOF.sol";
+
+/*
+ yarn deploy -n <network> -u MU04 -s MU04-01-Create-Implementations.sol
+*/
+contract MU01_CreateImplementations is Script {
+  function run() public {
+    address stableTokenXOF;
+    address governance = contracts.celoRegistry("Governance");
+
+    vm.startBroadcast(Chain.deployerPrivateKey());
+    {
+      stableTokenXOF = address(new StableTokenXOF(false));
+      StableTokenXOF(stableTokenXOF).transferOwnership(governance);
+    }
+    vm.stopBroadcast();
+
+    console2.log("----------");
+    console2.log("StableTokenXOF deployed at: ", stableTokenXOF);
+    console2.log("StableTokenXOF(%s) ownership transferred to %s", stableTokenXOF, governance);
+    console2.log("----------");
+  }
+}
