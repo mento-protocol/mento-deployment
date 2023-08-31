58d717c026c6bdd0753495aaa4bb20568cb8ff93
Bayological <6872903+bayological@users.noreply.github.com>
2023-08-31 14:48:49 -0500

    refactor: remove unnecessary code

--- script/upgrades/eXOF/eXOFChecks.sol
+++ script/upgrades/eXOF/eXOFChecks.sol
@@ -4,18 +4,12 @@ pragma experimental ABIEncoderV2;
 
 import { Test } from "forge-std/Test.sol";
 import { Script } from "script/utils/Script.sol";
-import { Contracts } from "script/utils/Contracts.sol";
 
-import { eXOFChecksBase } from "./eXOFChecks.base.sol";
 import { eXOFChecksSwap } from "./eXOFChecks.swap.sol";
 import { eXOFChecksVerify } from "./eXOFChecks.verify.sol";
 
-contract eXOFChecks is eXOFChecksBase {
-  using Contracts for Contracts.Cache;
-
+contract eXOFChecks {
   function run() public {
-    setUp();
-
     new eXOFChecksVerify().run();
     new eXOFChecksSwap().run();
   }
