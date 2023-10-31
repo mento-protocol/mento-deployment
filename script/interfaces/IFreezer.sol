a0618cf366f82ac216c1731d9b55ddcb98e6846f
philbow61 <philip.raetsch@gmail.com>
2023-10-31 15:22:59 +0100

    revert: last commit

--- script/interfaces/IFreezer.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.13;
-
-interface IFreezer {
-  function freeze(address target) external;
-
-  function unfreeze(address target) external;
-
-  function isFrozen(address account) external view returns (bool);
-}
