6c865942f7950d5793f5763f63b3aaf937a8c668
baroooo <baranseltekin@gmail.com>
2024-01-04 12:37:14 +0100

    feat: folder structure

---
+++ script/upgrades/GOV/deploy/GOV-01-Create-Nonupgradeable-Contracts.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+import { console2 } from "forge-std/Script.sol";
+import { Script } from "script/utils/Script.sol";
+import { Chain } from "script/utils/Chain.sol";
+
+/*
+ yarn deploy -n <network> -u GOV -s GOV-01-Create-Nonupgradeable-Contracts.sol
+*/
+contract GOV_CreateNonupgradeableContracts is Script {
+ 
+
+  function run() public {
+   
+  }
+}
