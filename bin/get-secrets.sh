b21cbd496fa640f62abcf6a27d1cef27d492a6ef
chapati <philip.paetz@me.com>
2024-02-09 10:39:53 +0100

    fix(get-secrets): added macOS-compatible path for `sed` util usage (#161)

--- bin/get-secrets.sh
+++ bin/get-secrets.sh
@@ -2,7 +2,7 @@
 
 ##############################################################################
 # Script for pulling secret keys from GCP secret manager.
-# Usage: ./bin/get-secrets.sh 
+# Usage: ./bin/get-secrets.sh
 ##############################################################################
 
 # Authenticate with Google Cloud
@@ -23,17 +23,23 @@ for SECRET_ID in $(echo "$SECRET_IDS" | tr ',' ' '); do
   SECRET_VALUE="$(gcloud secrets versions access latest --secret="$SECRET_ID" --project="$PROJECT_ID")"
 
   # Write the secret value to the .env file
-  
-  # If the secret name already exists in the .env file, replace the value with the new value. 
+
+  # If the secret name already exists in the .env file, replace the value with the new value.
   if grep -q "^$SECRET_NAME=" "$ENV_FILE"; then
-    sed -i "s/^$SECRET_NAME=.*/$SECRET_NAME=$SECRET_VALUE/g" "$ENV_FILE"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      # macOS sed syntax
+      sed -i "" "s/^$SECRET_NAME=.*/$SECRET_NAME=$SECRET_VALUE/g" "$ENV_FILE"
+    else
+      # GNU/Linux sed syntax
+      sed -i "s/^$SECRET_NAME=.*/$SECRET_NAME=$SECRET_VALUE/g" "$ENV_FILE"
+    fi
   else
     # If the .env file is not empty, append the secret name and value to the .env file.
     if [[ -s "$ENV_FILE" ]]; then
-      printf "\n%s=%s" "$SECRET_NAME" "$SECRET_VALUE" >> "$ENV_FILE"
+      printf "\n%s=%s" "$SECRET_NAME" "$SECRET_VALUE" >>"$ENV_FILE"
     else
       # If we don't have an .env, write the secret name and value to the .env file.
-      echo -n "$SECRET_NAME=$SECRET_VALUE" >> "$ENV_FILE"
+      echo -n "$SECRET_NAME=$SECRET_VALUE" >>"$ENV_FILE"
     fi
   fi
 done
