d1f9d2a2f1e615a77413738a843f07f570d2164c
bowd <dumbogdan@gmail.com>
2024-02-13 09:28:21 +0100

    feat: add command to verify celoscan

---
+++ bin/utils/etherscan.ts
@@ -0,0 +1,59 @@
+export const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+export enum VerificationStatus {
+  FAILED = "Fail - Unable to verify",
+  SUCCESS = "Pass - Verified",
+  PENDING = "Pending in queue",
+  ALREADY_VERIFIED = "Contract source code already verified",
+}
+
+type VerifyParams = {
+  api: string;
+  apiKey: string | undefined;
+  contract: string;
+  source: any;
+  target: string;
+  version: string;
+  args: string;
+};
+export async function verify({ api, apiKey, contract, source, target, version, args }: VerifyParams): Promise<string> {
+  const params = new URLSearchParams();
+  if (apiKey) {
+    params.append("apikey", apiKey);
+  }
+  params.append("module", "contract");
+  params.append("action", "verifysourcecode");
+  params.append("contractaddress", contract);
+  params.append("sourceCode", JSON.stringify(source));
+  params.append("codeformat", "solidity-standard-json-input");
+  params.append("contractname", target);
+  params.append("compilerversion", version);
+  params.append("constructorArguements", args);
+
+  const data = await fetch(api, {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded;charset=utf-8" },
+    body: params,
+  }).then(res => res.json());
+
+  return data.result;
+}
+
+export async function waitFor(api: string, apiKey: string | undefined, guid: string): Promise<string> {
+  while (true) {
+    await delay(3000);
+
+    const params = new URLSearchParams();
+    if (apiKey) {
+      params.append("apiKey", apiKey);
+    }
+    params.append("module", "contract");
+    params.append("action", "checkverifystatus");
+    params.append("guid", guid);
+
+    const data = await fetch(api + "?" + params).then(res => res.json());
+    if (data.result !== VerificationStatus.PENDING) {
+      return data.result;
+    }
+  }
+}
---
+++ bin/utils/sourcify.ts
@@ -0,0 +1,66 @@
+export const SOURCIFY_API = "https://sourcify.dev/server/";
+
+export async function check(chainId: number, contract: string): Promise<string> {
+  const data = await fetch(`${SOURCIFY_API}check-all-by-addresses?addresses=${contract}&chainIds=${chainId}`).then(
+    res => res.json(),
+  );
+  if (!data || !data[0]) {
+    return "false";
+  }
+  return data[0].status;
+}
+
+export async function files(chainId: number, contract: string): Promise<any> {
+  const data = await fetch(`${SOURCIFY_API}files/any/${chainId}/${contract}`).then(res => res.json());
+
+  return data.files;
+}
+
+export type Metadata = {
+  language: string;
+  settings: any;
+};
+type SourcifyOutput = {
+  metadata: Metadata;
+  version: string;
+  sources: any;
+  target: string;
+  constructorArgs: string;
+  libraryMap: Record<string, string>;
+};
+type FileContent = {
+  content: string;
+};
+type File = FileContent & {
+  name: string;
+  path: string;
+};
+
+export function parseFiles(files: any): SourcifyOutput {
+  const metadata = JSON.parse(files.find((file: File) => file.name === "metadata.json").content);
+  const constructorArgs = files.find((file: File) => file.name === "constructor-args.txt");
+  const libraryMapFile = files.find((file: File) => file.name === "library-map.json");
+  const libraryMap = libraryMapFile ? JSON.parse(libraryMapFile.content) : {};
+  const sourcesArray = files.filter(
+    (file: File) =>
+      file.name !== "metadata.json" && file.name !== "constructor-args.txt" && !file.name.endsWith("json"),
+  );
+  const version = `v${metadata.compiler.version}`;
+  const target = Object.entries(metadata.settings.compilationTarget)[0].join(":");
+  const sources: { [key: string]: FileContent } = {};
+
+  const prefix = sourcesArray.find((file: File) => file.path.match(/^.*\/sources\//))!.path.match(/^.*\/sources\//)[0];
+  for (const file of sourcesArray) {
+    const path = file.path.replace("sources/_", "sources/@").slice(prefix.length);
+    sources[path] = { content: file.content };
+  }
+
+  return {
+    metadata,
+    version,
+    target,
+    sources,
+    constructorArgs: constructorArgs ? constructorArgs.content.slice(2) : "",
+    libraryMap,
+  };
+}
