97e2164da8f9519f2c679e747b16918f051888df
Bayological <6872903+bayological@users.noreply.github.com>
2024-05-02 14:36:34 +0200

    feat: add scripts to deploy new stable token proxies (#176)
    ### Description
    
    - Adds deployment scripts for StableTokenCOPProxy & StableTokenKESProxy
    
    ### Other changes
    
    - None
    
    ### Tested
    
    - N/A
    
    ### Related issues
    
    - Fixes #174 
    
    ### Backwards compatibility
    
    N/A
    
    ### Documentation
    - N/A

---
+++ script/upgrades/cKES/deploy/cKES-00-Create-Proxies.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.5.13;
+
+import { Script } from "script/utils/Script.sol";
+import { Chain } from "script/utils/Chain.sol";
+import { console2 } from "forge-std/Script.sol";
+
+import { StableTokenKESProxy } from "mento-core-2.4.0/legacy/proxies/StableTokenKESProxy.sol";
+
+/*
+ yarn deploy -n <network> -u cKES -s cKES-00-Create-Proxies.sol
+*/
+contract cKES_CreateProxies is Script {
+  function run() public {
+    address payable stableTokenKESProxy;
+    address governance = contracts.celoRegistry("Governance");
+
+    vm.startBroadcast(Chain.deployerPrivateKey());
+    {
+      stableTokenKESProxy = address(new StableTokenKESProxy());
+      StableTokenKESProxy(stableTokenKESProxy)._transferOwnership(governance);
+    }
+    vm.stopBroadcast();
+
+    console2.log("----------");
+    console2.log("StableTokenKESProxy deployed at: ", stableTokenKESProxy);
+    console2.log("StableTokenKESProxy(%s) ownership transferred to %s", stableTokenKESProxy, governance);
+    console2.log("----------");
+  }
+}
