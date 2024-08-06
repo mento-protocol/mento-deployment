ba5378543c354f6407048e736be64b98e41d54cb
baroooo <baranseltekin@gmail.com>
2024-08-06 10:37:50 +0200

    feat: mento gov utils scripts (#198)
    * feat: mento gov utils scripts
    
    * fix: interfaces uses correct pragma
    
    * fix: simulation bug
    
    * feat: fixed solc version
    
    * feat: use internal lib functions
    
    * chore: fix linter
    
    * feat: remove sender from cgp script
    
    * chore: remove blank space

---
+++ script/utils/mento/Chain.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.18;
+
+import { Vm } from "forge-std/Vm.sol";
+
+library Chain {
+  address private constant VM_ADDRESS = address(bytes20(uint160(uint256(keccak256("hevm cheat code")))));
+  // solhint-disable-next-line const-name-snakecase
+  Vm public constant vm = Vm(VM_ADDRESS);
+
+  uint256 public constant NETWORK_ANVIL = 0;
+
+  uint256 public constant NETWORK_CELO_CHAINID = 42220;
+  string public constant NETWORK_CELO_CHAINID_STRING = "42220";
+  string public constant NETWORK_CELO_RPC = "celo";
+  string public constant NETWORK_CELO_PK_ENV_VAR = "MENTO_DEPLOYER_PK";
+  address public constant GOVERNANCE_FACTORY_CELO = 0xee6CE2dbe788dFC38b8F583Da86cB9caf2C8cF5A;
+
+  uint256 public constant NETWORK_BAKLAVA_CHAINID = 62320;
+  string public constant NETWORK_BAKLAVA_CHAINID_STRING = "62320";
+  string public constant NETWORK_BAKLAVA_RPC = "baklava";
+  string public constant NETWORK_BAKLAVA_PK_ENV_VAR = "BAKLAVA_DEPLOYER_PK";
+  address public constant GOVERNANCE_FACTORY_BAKLAVA = 0xe23A28a92B95c743fC0F09c16a6b2E6D59F234Fa;
+
+  uint256 public constant NETWORK_ALFAJORES_CHAINID = 44787;
+  string public constant NETWORK_ALFAJORES_CHAINID_STRING = "44787";
+  string public constant NETWORK_ALFAJORES_RPC = "alfajores";
+  string public constant NETWORK_ALFAJORES_PK_ENV_VAR = "ALFAJORES_DEPLOYER_PK";
+  address public constant GOVERNANCE_FACTORY_ALFAJORES = 0x96Fe03DBFEc1EB419885a01d2335bE7c1a45e33b;
+
+  /**
+   * @notice Get the current chainId
+   * @return _chainId the chain id
+   */
+  function id() internal view returns (uint256 _chainId) {
+    // solhint-disable-next-line no-inline-assembly
+    assembly {
+      _chainId := chainid()
+    }
+  }
+
+  function idString() internal view returns (string memory) {
+    uint256 _chainId = id();
+    if (_chainId == NETWORK_CELO_CHAINID) return NETWORK_CELO_CHAINID_STRING;
+    if (_chainId == NETWORK_BAKLAVA_CHAINID) return NETWORK_BAKLAVA_CHAINID_STRING;
+    if (_chainId == NETWORK_ALFAJORES_CHAINID) return NETWORK_ALFAJORES_CHAINID_STRING;
+    revert("unexpected network");
+  }
+
+  function rpcToken() internal view returns (string memory) {
+    uint256 _chainId = id();
+    if (_chainId == NETWORK_CELO_CHAINID) return NETWORK_CELO_RPC;
+    if (_chainId == NETWORK_BAKLAVA_CHAINID) return NETWORK_BAKLAVA_RPC;
+    if (_chainId == NETWORK_ALFAJORES_CHAINID) return NETWORK_ALFAJORES_RPC;
+    revert("unexpected network");
+  }
+
+  function deployerPrivateKey() internal view returns (uint256) {
+    uint256 _chainId = id();
+    if (_chainId == NETWORK_CELO_CHAINID) return vm.envUint(NETWORK_CELO_PK_ENV_VAR);
+    if (_chainId == NETWORK_BAKLAVA_CHAINID) return vm.envUint(NETWORK_BAKLAVA_PK_ENV_VAR);
+    if (_chainId == NETWORK_ALFAJORES_CHAINID) return vm.envUint(NETWORK_ALFAJORES_PK_ENV_VAR);
+    revert("unexpected network");
+  }
+
+  function governanceFactory() internal view returns (address) {
+    uint256 _chainId = id();
+    if (_chainId == NETWORK_CELO_CHAINID) return GOVERNANCE_FACTORY_CELO;
+    if (_chainId == NETWORK_BAKLAVA_CHAINID) return GOVERNANCE_FACTORY_BAKLAVA;
+    if (_chainId == NETWORK_ALFAJORES_CHAINID) return GOVERNANCE_FACTORY_ALFAJORES;
+    revert("unexpected network");
+  }
+
+  function deployerAddr() internal view returns (address payable) {
+    return payable(address(uint160(vm.addr(deployerPrivateKey()))));
+  }
+
+  /**
+   * @notice Setup a fork environment for the current chain
+   */
+  function fork() internal {
+    uint256 forkId = vm.createFork(rpcToken());
+    vm.selectFork(forkId);
+  }
+
+  function isCelo() internal view returns (bool) {
+    return id() == NETWORK_CELO_CHAINID;
+  }
+
+  function isBaklava() internal view returns (bool) {
+    return id() == NETWORK_BAKLAVA_CHAINID;
+  }
+
+  function isAlfajores() internal view returns (bool) {
+    return id() == NETWORK_ALFAJORES_CHAINID;
+  }
+}
---
+++ script/utils/mento/ExecuteProposal.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.18;
+
+import { Script } from "./Script.sol";
+import { IGovernanceFactory } from "../../interfaces/IGovernanceFactory.sol";
+import { IGovernor } from "../../interfaces/IGovernor.sol";
+import { Chain } from "./Chain.sol";
+
+contract ExecuteProposal is Script {
+  function run(uint256 proposalId) public {
+    address governance = IGovernanceFactory(Chain.governanceFactory()).mentoGovernor();
+
+    vm.startBroadcast(vm.envUint("MENTO_DEPLOYER_PK"));
+    {
+      IGovernor(governance).execute(proposalId);
+    }
+    vm.stopBroadcast();
+  }
+}
