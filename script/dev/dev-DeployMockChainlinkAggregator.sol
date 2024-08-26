667e60a1629003db15dcad38cb75afca13eec3e4
bowd <dumbogdan@gmail.com>
2024-08-26 14:18:03 +0200

    fix: naming in DeployMockChainlinkAggregator

--- script/dev/dev-DeployMockChainlinkAggregator.sol
+++ script/dev/dev-DeployMockChainlinkAggregator.sol
@@ -12,13 +12,13 @@ import { MockChainlinkAggregator } from "contracts/MockChainlinkAggregator.sol";
  * Usage: yarn script:dev -n alfajores -s DeployMockChainlinkAggregator -r "run(string)" PHPUSD
  * Used to deploy mock Chainlink Aggregators to Alfajores and Baklava to be used
  * in testnet relayers to mimic mainnet more closely.
- * When using This script, change the string inside the
+ * @dev After deploying the script save the broadcast file as run-{rateFeed}.json
  */
 contract DeployMockChainlinkAggregator is Script {
-  function run(string memory rateDescription) public {
+  function run(string memory rateFeed) public {
     vm.startBroadcast(ChainLib.deployerPrivateKey());
     {
-      new MockChainlinkAggregator(rateDescription);
+      new MockChainlinkAggregator(rateFeed);
     }
     vm.stopBroadcast();
   }
