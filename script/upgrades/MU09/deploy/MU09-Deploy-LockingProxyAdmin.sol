2a50ba42403db4df77449597ad82ef8dae3ee3ac
Bayological <6872903+bayological@users.noreply.github.com>
2024-11-07 15:00:23 -0500

    feat: add deployment script

---
+++ script/upgrades/MU09/deploy/MU09-Deploy-LockingProxyAdmin.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// solhint-disable contract-name-camelcase
+pragma solidity ^0.8.18;
+
+import { console } from "forge-std-next/console.sol";
+import { Script } from "script/utils/mento/Script.sol";
+import { Chain as ChainLib } from "script/utils/mento/Chain.sol";
+import { Contracts } from "script/utils/mento/Contracts.sol";
+import { ProxyDeployerLib } from "mento-core-2.5.0/governance/deployers/ProxyDeployerLib.sol";
+
+interface IOwnableLite {
+    function transferOwnership(address newOwner) external;
+}
+
+contract MU09_Deploy_LockingProxyAdmin is Script {
+  using Contracts for Contracts.Cache;
+
+  function run() public { 
+    address mentoLabsMultisig = contracts.dependency("MentoLabsMultisig");
+    require(mentoLabsMultisig != address(0), "MentoLabsMultisig address not found");
+
+    vm.startBroadcast(ChainLib.deployerPrivateKey());
+    {
+      IOwnableLite proxyAdmin = ProxyDeployerLib.deployAdmin();
+      console.log("Deployed ProxyAdmin for Locking at: %s", address(proxyAdmin));
+
+      proxyAdmin.transferOwnership(mentoLabsMultisig);
+      console.log("Transferred LockingProxyAdmin ownership to MentoLabsMultisig: %s", mentoLabsMultisig);
+    }
+    vm.stopBroadcast();
+  }
+}
