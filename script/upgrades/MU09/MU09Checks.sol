5b2a9b8e3e8e01b3ce5b03fa6e5c4b5e303df390
Bayological <6872903+bayological@users.noreply.github.com>
2024-11-08 14:49:25 -0500

    refactor: update checks

--- script/upgrades/MU09/MU09Checks.sol
+++ script/upgrades/MU09/MU09Checks.sol
@@ -9,16 +9,18 @@ import { Test } from "forge-std/Test.sol";
 
 import { IGovernanceFactory } from "script/interfaces/IGovernanceFactory.sol";
 
+interface IProxyAdminLite {
+  function getProxyAdmin(address proxy) external view returns (address);
+}
+
 interface IOwnableLite {
   function owner() external view returns (address);
-
-  function transferOwnership(address recipient) external;
 }
 
 contract MU09Checks is GovernanceScript, Test {
   using Contracts for Contracts.Cache;
 
-  address public lockingProxyAdmin;
+  address public newLockingProxyAdmin;
   address public lockingProxy;
   address public mentoLabsMultisig;
 
@@ -31,8 +33,8 @@ contract MU09Checks is GovernanceScript, Test {
     require(mentoLabsMultisig != address(0), "MentoLabsMultisig address not found");
 
     // Get newly deployed LockingProxyAdmin address
-    lockingProxyAdmin = contracts.deployed("ProxyAdmin");
-    require(lockingProxyAdmin != address(0), "LockingProxyAdmin address not found");
+    newLockingProxyAdmin = contracts.deployed("ProxyAdmin");
+    require(newLockingProxyAdmin != address(0), "LockingProxyAdmin address not found");
 
     // Get and set the governance factory
     address governanceFactoryAddress = contracts.deployed("GovernanceFactory");
@@ -49,22 +51,22 @@ contract MU09Checks is GovernanceScript, Test {
     prepare();
 
     verifyLockingProxyAdminOwnership();
-    verifyLockingProxyOwnership();
+    verifyLockingProxyAdminIsNewLockingProxyAdmin();
   }
 
   function verifyLockingProxyAdminOwnership() public {
     console.log("\n== Verifying locking proxy admin ownership: ==");
 
-    address lockingProxyAdminOwner = IOwnableLite(lockingProxyAdmin).owner();
+    address lockingProxyAdminOwner = IOwnableLite(newLockingProxyAdmin).owner();
     require(lockingProxyAdminOwner == mentoLabsMultisig, "LockingProxyAdmin owner is not MentoLabsMultisig");
     console.log(unicode"🟢 LockingProxyAdmin owner is MentoLabsMultisig: %s", lockingProxyAdminOwner);
   }
 
-  function verifyLockingProxyOwnership() public {
-    console.log("\n== Verifying locking proxy ownership: ==");
+  function verifyLockingProxyAdminIsNewLockingProxyAdmin() public {
+    console.log("\n== Verifying locking proxy admin is new locking proxy admin: ==");
 
-    address lockingProxyOwner = IOwnableLite(lockingProxy).owner();
-    require(lockingProxyOwner == lockingProxyAdmin, "LockingProxy owner is not LockingProxyAdmin");
-    console.log(unicode"🟢 LockingProxy owner is LockingProxyAdmin: %s", lockingProxyOwner);
+    address lockingProxyAdmin = IProxyAdminLite(newLockingProxyAdmin).getProxyAdmin(lockingProxy);
+    require(lockingProxyAdmin == newLockingProxyAdmin, "LockingProxyAdmin is not the new LockingProxyAdmin");
+    console.log(unicode"🟢 LockingProxyAdmin is new LockingProxyAdmin: %s", lockingProxyAdmin);
   }
 }
