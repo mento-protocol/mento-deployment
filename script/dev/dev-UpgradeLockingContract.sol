431a9a15b5032647bb974722999cf89539266cef
Ryan Noble <ryanjnoble@gmail.com>
2025-02-07 17:44:11 +0200

    feat: updated for latest version

--- script/dev/dev-UpgradeLockingContract.sol
+++ script/dev/dev-UpgradeLockingContract.sol
@@ -5,9 +5,7 @@ import { console2 } from "forge-std/Script.sol";
 import { Script } from "script/utils/mento/Script.sol";
 import { Chain as ChainLib } from "script/utils/mento/Chain.sol";
 import { Contracts } from "script/utils/mento/Contracts.sol";
-import { Locking } from "mento-core-2.6.1/governance/locking/Locking.sol";
-import { IGovernanceFactory } from "script/interfaces/IGovernanceFactory.sol";
-import { IERC20Upgradeable } from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/IERC20Upgradeable.sol";
+import { Locking } from "mento-core-2.6.2/governance/locking/Locking.sol";
 
 /**
  * Usage: yarn script:dev -n alfajores -s UpgradeLockingContract -r "run()"
@@ -20,27 +18,11 @@ contract UpgradeLockingContract is Script {
   function run() public {
     contracts.load("MUGOV-00-Create-Factory", "latest");
     address lockingV2;
-    address mentoToken = IGovernanceFactory(contracts.deployed("GovernanceFactory")).mentoToken();
     vm.startBroadcast(ChainLib.deployerPrivateKey());
     {
-      lockingV2 = address(new Locking()); // TODO: Update once V2.6.2 is out
+      lockingV2 = address(new Locking(true));
       console2.log("----------");
       console2.log("LockingV2 deployed at: ", lockingV2);
-      console2.log("----------");
-
-      // These values are taken from mainnet and initialized the proxy initially
-      uint32 startingPointWeek = 212;
-      uint32 minCliffPeriod = 0;
-      uint32 minSlopePeriod = 1;
-
-      Locking(lockingV2).__Locking_init(
-        IERC20Upgradeable(mentoToken),
-        startingPointWeek,
-        minCliffPeriod,
-        minSlopePeriod
-      );
-
-      console2.log("LockingV2 initialized");
       console2.log("----------");
     }
     vm.stopBroadcast();
