9d2c9a0205689301c7d244e9f536de8f2406dbdd
philbow61 <philip.raetsch@gmail.com>
2025-02-07 16:27:33 +0100

    revert: deployment script changes

--- script/upgrades/cGHS/deploy/cGHS-00-Deploy-Proxy.sol
+++ script/upgrades/cGHS/deploy/cGHS-00-Deploy-Proxy.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.5.13;
 import { Script } from "script/utils/Script.sol";
 import { Chain } from "script/utils/Chain.sol";
 import { console2 } from "forge-std/Script.sol";
-import { IGovernanceFactory } from "script/interfaces/IGovernanceFactory.sol";
 
 import { StableTokenGHSProxy } from "mento-core-2.6.0/tokens/StableTokenGHSProxy.sol";
 
@@ -15,19 +14,18 @@ contract cGHS_DeployProxy is Script {
   function run() public {
     address payable stableTokenGHSProxy;
 
-    address governanceFactory = contracts.deployed("GovernanceFactory");
-    address timelockProxy = IGovernanceFactory(governanceFactory).governanceTimelock();
+    address governance = contracts.celoRegistry("Governance");
 
     vm.startBroadcast(Chain.deployerPrivateKey());
     {
       stableTokenGHSProxy = address(new StableTokenGHSProxy());
-      StableTokenGHSProxy(stableTokenGHSProxy)._transferOwnership(timelockProxy);
+      StableTokenGHSProxy(stableTokenGHSProxy)._transferOwnership(governance);
     }
     vm.stopBroadcast();
 
     console2.log("----------");
     console2.log("StableTokenGHSProxy deployed at: ", stableTokenGHSProxy);
-    console2.log("StableTokenGHSProxy(%s) ownership transferred to %s", stableTokenGHSProxy, timelockProxy);
+    console2.log("StableTokenGHSProxy(%s) ownership transferred to %s", stableTokenGHSProxy, governance);
     console2.log("----------");
   }
 }
