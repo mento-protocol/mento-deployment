9d87adfa00b4ff5e9cfa77f9175029b5b4638f5a
Nelson Taveras <4562733+nvtaveras@users.noreply.github.com>
2025-02-11 16:28:19 +0100

    refactor: move new locking deployment script to upgrades

--- script/upgrades/L2Upgrade/deploy/L2Upgrade-00-Deploy-NewLockingImplementation.sol
+++ script/upgrades/L2Upgrade/deploy/L2Upgrade-00-Deploy-NewLockingImplementation.sol
@@ -4,17 +4,12 @@ pragma solidity 0.8.18;
 import { console2 } from "forge-std/Script.sol";
 import { Script } from "script/utils/mento/Script.sol";
 import { Chain as ChainLib } from "script/utils/mento/Chain.sol";
-import { Contracts } from "script/utils/mento/Contracts.sol";
 import { Locking } from "mento-core-2.6.2/governance/locking/Locking.sol";
 
 /**
- * Usage: yarn script:dev -n celo -s DeployNewLockingImplementation
- * Used to deploy the Locking V2 implementation
- * ===========================================================
+ yarn deploy -n <network> -u L2Upgrade -s L2Upgrade-00-Deploy-NewLockingImplementation.sol
  */
 contract DeployNewLockingImplementation is Script {
-  using Contracts for Contracts.Cache;
-
   function run() public {
     address lockingV2;
     vm.startBroadcast(ChainLib.deployerPrivateKey());
