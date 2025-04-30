217c7f5eaa1912501f8af5766c9fab4b9f75f818
nvtaveras <4562733+nvtaveras@users.noreply.github.com>
2025-04-30 14:06:47 +0200

    feat: add FX02 MD url (#273)

--- script/upgrades/FX02/FX02.sol
+++ script/upgrades/FX02/FX02.sol
@@ -90,7 +90,7 @@ contract FX02 is IMentoUpgrade, GovernanceScript {
 
     vm.startBroadcast(Chain.deployerPrivateKey());
     {
-      createProposal(_transactions, "todo", governance);
+      createProposal(_transactions, "https://github.com/celo-org/governance/blob/main/CGPs/cgp-0180.md", governance);
     }
     vm.stopBroadcast();
   }
