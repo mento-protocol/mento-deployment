8381ab4b1e27a1e8c3bcf74414d1a9cde08e7820
Nelson Taveras <4562733+nvtaveras@users.noreply.github.com>
2025-04-30 16:44:53 +0200

    feat: add FX03 MD url

--- script/upgrades/FX03/FX03.sol
+++ script/upgrades/FX03/FX03.sol
@@ -116,8 +116,7 @@ contract FX03 is IMentoUpgrade, GovernanceScript {
 
     vm.startBroadcast(Chain.deployerPrivateKey());
     {
-      // TODO: confirm proposal github link
-      createProposal(_transactions, "TODO", governance);
+      createProposal(_transactions, "https://github.com/celo-org/governance/blob/main/CGPs/cgp-0182.md", governance);
     }
     vm.stopBroadcast();
   }
